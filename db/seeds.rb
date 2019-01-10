41f3c2c02608e3ce56d597965d753040e23fe8b0
Steven Z <Steven@Zhao.io>
2019-01-09 22:11:22 -0800

    Add product seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+Product.create! id: 1, title: "Iron Helmet", price: 50.00, inventory_count: 25
+Product.create! id: 2, title: "Hylian Shield", price: 1333.37, inventory_count: 1
+Product.create! id: 3, title: "Leather Boots", price: 45.50, inventory_count: 10
+Product.create! id: 4, title: "Mythical Chainmail", price: 34.00, inventory_count: 0
