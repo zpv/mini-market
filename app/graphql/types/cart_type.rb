489a9c35814ddd8a5a9ad629db8c78074abc9bc0
Steven Z <Steven@Zhao.io>
2019-01-20 23:00:11 -0800

    Remove Cart ID Field

--- app/graphql/types/cart_type.rb
+++ app/graphql/types/cart_type.rb
@@ -2,8 +2,6 @@ module Types
   class CartType < Types::BaseObject
     description 'Holds items a user has added in a session'
 
-    field :id, ID, null: false
-
     field :subtotal, Float, null: false
     field :cart_items, [CartItemType], null: true
   end
